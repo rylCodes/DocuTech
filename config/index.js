c321e8443fb1f954518c22d26b20872e34829765
rylCodes <andresdaryl.94@gmail.com>
2024-06-19 23:06:32 +0800

    include http://127.0.0.1:8000/ as localhost base endpoint

--- config/index.js
+++ config/index.js
@@ -1,5 +1,9 @@
 const dev = process.env.NODE_ENV !== "production";
 
 export const server = dev
-  ? "http://ryltechdocs.test"
+  ? "http://127.0.0.1:8000"
   : "http://somewebsitedomain.com";
+
+// export const server = dev
+// ? "http://ryltechdocs.test"
+// : "http://somewebsitedomain.com";
